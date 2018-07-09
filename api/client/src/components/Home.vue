61214efb1ebf7666c1d6ec321782348c9319ae77
Omayeli Arenyeka <aomayeli@gmail.com>
2018-07-08 23:17:27 -0400

    Displaying words

---
+++ api/client/src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <ul v-if="words && words.length">
+       <li v-for="word of words">
+         <span>{{word.word}}</span>
+       </li>
+     </ul>
+  </div>
+</template>
+
+<script>
+const API = 'http://localhost:3000/api/words';
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      words: [],
+    }
+  },
+  created() {
+    fetch(API)
+    .then(res => res.json())
+    .then(res => this.words = res);
+  },
+};
+</script>
+<style lang="scss">
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    height: 100%;
+    list-style: none;
+
+    li {
+      padding: 15px;
+    }
+  }
+</style>
--- api/client/src/main.js
+++ api/client/src/main.js
@@ -1,33 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-const API = 'http://localhost:3000/api/words';
 
 Vue.config.productionTip = false;
 
 const newApp = new Vue({
   el: '#app',
   router,
-  data:{
-		words:[],
-		word:{
-			id:'',
-			name:'',
-			gender:''
-		}
-	},
   render: h => h(App),
-  created:function() {
-		this.getWords();
-	},
-  methods:{
-		getWords:function() {
-			fetch(API)
-			.then(res => () {
-        res.json()
-        console.log(res.json)
-      })
-			.then(res => this.words = res);
-		},
-	}
 });
