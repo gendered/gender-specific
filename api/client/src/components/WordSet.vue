8c38a16bbf457f629d9bfa25729df826a3040b52
Omayeli Arenyeka <aomayeli@gmail.com>
2018-07-11 17:11:01 -0400

    Adding new route for words, each word on home page is a link.

---
+++ api/client/src/components/WordSet.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <section :class="'word-set ' + word.gender">
+      <span>{{ word.word }}</span>
+      <span>{{ word.definition }}</span>
+    </section>
+    <!-- <section :class="'word-set ' + word.gender">
+      <span>{{ word.word }}</span>
+      <span>{{ word.definition }}</span>
+    </section> -->
+  </div>
+</template>
+
+<script>
+  const API = 'http://localhost:3000/api/words/';
+
+  export default {
+    name: 'WordSet',
+    props: ['id'],
+    data() {
+      return {
+        word: {},
+      }
+    },
+    created() {
+      fetch(API + this.id)
+      .then(res => res.json())
+      .then(res => this.word = res);
+    },
+  };
+</script>
