5c9a07c0fd04446e57b9cc661623003cbe9ffc4f
Omayeli Arenyeka <aomayeli@gmail.com>
2018-07-11 16:39:41 -0400

    Separating css, js and html

--- api/client/src/App.vue
+++ api/client/src/App.vue
@@ -8,7 +8,7 @@
     <router-view/>
   </div>
 </template>
-
+<style src="./App.scss"></style>
 <script>
   import SearchFilter from '@/components/SearchFilter'
   export default {
@@ -17,24 +17,3 @@
     },
   }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100vh;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
