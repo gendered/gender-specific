a09be4c9129f8f0c39c739150a853fa82a751765
Omayeli Arenyeka <aomayeli@gmail.com>
2018-07-18 14:05:48 -0400

    Adding button component

---
+++ api/client/src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type="submit" @click="clicked :value="text" />
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: ['text'],
+  methods: {
+    clicked() {
+    }
+  },
+};
+</script>
