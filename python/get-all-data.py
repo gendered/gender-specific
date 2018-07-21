800000c9629810f874f19d142d7ce5f73248a83d
Omayeli Arenyeka <aomayeli@gmail.com>
2018-07-21 09:40:17 -0400

    More filters

--- python/get-all-data.py
+++ python/get-all-data.py
@@ -28,11 +28,11 @@ apiKey = os.getenv('API_KEY')
 client = swagger.ApiClient(apiKey, apiUrl)
 
 femaleTermsArr = ['woman', 'female', 'girl', 'lady', 'women', 'mother', 'daughter', 'wife']
-femaleTerms = r'\bwoman\b|\bfemale\b|\bgirl\b|\bgirls\b|\bwomen\b|\blady\b|\bmother\b|\bdaughter\b|\bwife\b'
+femaleTerms = r'\b[\w-]*woman\b|\bfemale\b|\b[\w-]girl\b|\bgirls\b|\b[\w-]*women\b|\blady\b|\b[\w-]*mother\b|\b[\w-]*daughter\b|\bwife\b'
 femaleRegex = re.compile(femaleTerms)
 
 maleTermsArr = ['man', 'male', 'boy', 'men', 'son', 'father', 'husband']
-maleTerms = r'\bman\b|\bmale\b|\bboy\b|\bmen\b|\bboys\b|\bson\b|\bfather\b|\bhusband\b'
+maleTerms = r'\bman\b|\bmale\b|\bboy\b|\bmen\b|\bboys\b|\bson\b|\b[\w-]*father\b|\bhusband\b'
 maleRegex = re.compile(maleTerms)
 allWords = []
 wordSet = set(['woman', 'female', 'girl', 'lady', 'man', 'male', 'boy', 'mother', 'daughter', 'son', 'father', 'husband', 'wife'])
@@ -93,60 +93,65 @@ def getWordDefinition(word):
 		definition = getDef(word)
 	return definition
 
-def filterWordByDefinition(definition, startIndex, endIndex):
-	def hasWordsToExclude():
-		arr = ['hormone', 'sperm', 'animal', 'organ', 'male or female', 'man or woman']
-		for item in arr:
-			if item in definition:
-				return True
-		return False
+def filterWordByDefinition(word, definition, startIndex, endIndex):
+    def hasWordsToExclude():
+        arr = ['hormone', 'sperm', 'animal', 'organ', 'male or female', 'man or woman']
+        for item in arr:
+            if item in definition:
+                return True
+        return False
 
-	# remove entries with animals in definition
-	def isThereAnimal():
-		for animal in animals:
-			animal = animal.lower()
-			if animal in definition:
-				return True
-		return False
+    # remove entries with animals in definition
+    def isThereAnimal():
+        for animal in animals:
+            animal = animal.lower()
+            if animal in definition:
+                return True
+        return False
 
-	def preprocess(sentence):
-		sentence = sentence.lower()
-		translator = str.maketrans('', '', string.punctuation)
-		return sentence.translate(translator)
+    def preprocess(sentence):
+        sentence = sentence.lower()
+        translator = str.maketrans('', '', string.punctuation)
+        return sentence.translate(translator)
 
-	def sentenceIsRightStructure():
-		# trim
-		trimmedDefinition = definition[0:endIndex]
-		# remove a and an
-		defs = re.sub(r'\ba\b|\ban\b|\bthe\b', '', trimmedDefinition)
-		cleanDefinition = preprocess(defs)
-		# part of speech tagger
-		text = word_tokenize(cleanDefinition)
-		tags = nltk.pos_tag(text)
-		# filter adjectives and adverbs
-		tags = [tag for tag in tags if 'JJ' not in tag[1] and 'RB' not in tag[1]]
-		# gendered term is the last in the string so it'll be the last in the array
-		length = len(tags)
-		# so the term before will be at this location
-		# check if it's a preposition or verb before gendered term
-		if length <= 1:
-			return True
-		else:
-			termOne = tags[length-2][1]
-			if termOne == 'IN' or 'VB' in termOne:
-				return False
-			if length > 2:
-				termTwo = tags[length-3][1]
-				if termTwo == 'IN' or 'VB' in termTwo:
-					return False
-		return True
+    def sentenceIsRightStructure():
+        # trim
+        trimmedDefinition = definition[0:endIndex]
+        # remove a and an
+        defs = re.sub(r'\ba\b|\ban\b|\bthe\b', '', trimmedDefinition)
+        cleanDefinition = preprocess(defs)
+        # part of speech tagger
+        text = word_tokenize(cleanDefinition)
+        tags = nltk.pos_tag(text)
+        # gendered term is the last in the string so it'll be the last in the array
+        length = len(tags)
+        # so the term before will be at this location
+        # check if it's a preposition or verb before gendered term
+        if length <= 1:
+            return True
+        else:
+            posOne = tags[length-2][1]
+            termOne = tags[length-2][0]
+            if termOne == 'to':
+                if length > 2:
+                    if tags[length-3][0] == 'given' or tags[length-3][0] == 'applied':
+                        return True
+            if posOne == 'IN' or 'VB' in posOne:
+                return False
 
-	if not isThereAnimal() and not hasWordsToExclude() and sentenceIsRightStructure():
-		return True
-	else:
-		return False
+            return True
 
 
+    if not isThereAnimal() and not hasWordsToExclude() and sentenceIsRightStructure():
+        return True
+    else:
+        return False
+
+def getWordPattern(gender):
+    if gender == 'female':
+        return re.compile(r'\b[\w-]*woman\b|\b[\w-]girl\b|\bgirls\b|\b[\w-]*women\b|\blady\b|\b[\w-]*mother\b|\b[\w-]*daughter\b|\bwife\b')
+    else:
+        return re.compile(r'\b[\w-]*man\b|\b[\w-]*boy\b|\bmen\b|\bboys\b|\bson\b|\b[\w-]*father\b|\bhusband\b')
 
 def getWordnik():
 	wordsApi = WordsApi.WordsApi(client)
@@ -159,6 +164,17 @@ def getWordnik():
 				word = result.word.lower()
 				definition = result.text
 				if (word not in wordSet):
+                    termsInWord = getWordPattern(gender).search(word)
+                    # get index of term
+                    if termsInWord is not None:
+                        wordSet.add(word)
+                        words.append({
+                            'word': word,
+                            'definition': definition,
+                            'gender': gender,
+                            'tags': [source]
+                        })
+                        continue
 					termsInString = pattern.search(definition)
 					if termsInString is not None:
 						startIndex = termsInString.start(0)
@@ -190,6 +206,17 @@ def getDatamuse():
 			for result in results:
 				word = result['word'].lower()
 				if (word not in wordSet):
+                    termsInWord = getWordPattern(gender).search(word)
+                    # get index of term
+                    if termsInWord is not None:
+                        wordSet.add(word)
+                        words.append({
+                            'word': word,
+                            'definition': definition,
+                            'gender': gender,
+                            'tags': [source]
+                        })
+                        continue
 					# check if it's a noun
 					if ('tags' in result):
 						if ('n' in result['tags']):
@@ -217,38 +244,49 @@ def getDatamuse():
 	print ('datamuse done')
 
 def getWebster():
-	with open('data/webster/dictionary.json', 'r') as f:
-		results = json.load(f)
-	source = 'webster'
+    with open('data/webster/dictionary.json', 'r') as f:
+    	results = json.load(f)
+    source = 'webster'
 
-	def bucket(pattern, gender):
-		words = []
-		for result in results:
-			# to-do: strip definition if it's too long
-			definition = results[result]
-			termsInString = pattern.search(definition.lower())
-			# get index of term
-			if termsInString is not None:
-				result = result.lower()
-				if (result not in wordSet):
-					startIndex = termsInString.start(0)
-					endIndex = termsInString.end(0)
-					if filterWordByDefinition(definition, startIndex, endIndex):
-						# get part of speech
-						for ss in wn.synsets(result):
-							pos = ss.pos()
-							if ('n' in pos):
-								wordSet.add(result)
-								words.append({
-									'word': result,
-									'definition': definition,
-									'gender': gender,
-									'tags': [source]
-								})
+    def bucket(pattern, gender):
+        words = []
+        for result in results:
+            # to-do: strip definition if it's too long
+            definition = results[result]
+            termsInString = pattern.search(definition.lower())
+            word = result.lower()
+            termsInWord = getWordPattern(gender).search(word)
+            if (word not in wordSet):
+                if termsInWord is not None:
+                        wordSet.add(word)
+                        words.append({
+                            'word': word,
+                            'definition': definition,
+                            'gender': gender,
+                            'tags': [source]
+                        })
+                        continue
+                if termsInString is not None:
+                    if (word not in wordSet):
+                        startIndex = termsInString.start(0)
+                        endIndex = termsInString.end(0)
+                        if filterWordByDefinition(result, definition, startIndex, endIndex):
+                            # get part of speech
+                            for ss in wn.synsets(result):
+                                pos = ss.pos()
+                                if ('n' in pos):
+                                    wordSet.add(result)
+                                    words.append({
+                                        'word': result,
+                                        'definition': definition,
+                                        'gender': gender,
+                                        'tags': [source]
+                                    })
 
-	bucket(femaleRegex, 'female')
-	bucket(maleRegex, 'male')
-	print ('webster done')
+
+    bucket(femaleRegex, 'female')
+    bucket(maleRegex, 'male')
+    print ('webster done')
 
 def getGSFull():
 	with open('data/gender_specific_full.json', 'r') as f:
@@ -259,8 +297,18 @@ def getGSFull():
 	def bucket(pattern, gender):
 		words = []
 		for result in results:
-			result = result.lower()
-			if (result not in wordSet):
+			word = result.lower()
+			if (word not in wordSet):
+                termsInWord = getWordPattern(gender).search(word)
+                if termsInWord is not None:
+                    wordSet.add(word)
+                    words.append({
+                        'word': word,
+                        'definition': definition,
+                        'gender': gender,
+                        'tags': [source]
+                    })
+                    continue
 				definition = getWordDefinition(result)
 				if (definition is not None and definition != ' '):
 					termsInString = pattern.search(definition)
