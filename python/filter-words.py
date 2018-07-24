07daae380d07ae5d2c00810bf3556e0d048776e3
Omayeli Arenyeka <aomayeli@gmail.com>
2018-07-24 18:23:00 -0400

    Test with google news vectors

--- python/filter-words.py
+++ python/filter-words.py
@@ -3,12 +3,11 @@ from gensim.models import KeyedVectors, word2vec
 filename = 'models/GoogleNews-vectors-negative300.bin'
 model = KeyedVectors.load_word2vec_format(filename, binary=True)
 
-
-from gensim.scripts.glove2word2vec import glove2word2vec
-glove_input_file = "models/glove.txt"
-word2vec_output_file = "word2vec.txt"
-glove2word2vec(glove_input_file, word2vec_output_file)
-m = KeyedVectors.load_word2vec_format(word2vec_output_file, binary=False)
+# from gensim.scripts.glove2word2vec import glove2word2vec
+# glove_input_file = "models/glove.txt"
+# word2vec_output_file = "models/word2vec.txt"
+# glove2word2vec(glove_input_file, word2vec_output_file)
+# m = KeyedVectors.load_word2vec_format(word2vec_output_file, binary=False)
 
 with open('words/unfiltered/all-unfiltered.json') as f:
     all_words = json.load(f)
@@ -49,10 +48,6 @@ def filterWords(arr):
                 m_sim = sims['m_sim']
 
                 if (f_sim < 0.1 and m_sim < 0.1):
-                    print(word)
-                    print('female', f_sim)
-                    print('male', m_sim)
-                    entry['strength'] = [m_sim, f_sim]
                     not_strong.append(entry)
                     continue
                 elif f_sim > 0.1 and f_sim > m_sim:
@@ -65,6 +60,6 @@ def filterWords(arr):
                 all.append(entry)
 
 filterWords(all_words)
-print(len(not_strong))
+print('google news vectors', len(not_strong))
 writeToJson('words/not_strong', not_strong)
 writeToJson('words/all', all)
