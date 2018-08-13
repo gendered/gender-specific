e096d05d1d8f338a952e276d303a87e780fead01
Omayeli Arenyeka <aomayeli@gmail.com>
2018-08-13 18:57:36 -0400

    Adding new api for getting synonyms

--- syn-set.py
+++ syn-set.py
@@ -2,6 +2,7 @@ import json
 import os
 from pprint import pprint
 from vocabulary.vocabulary import Vocabulary as vb
+from thesaurus import Word
 from nltk.corpus import wordnet
 import os
 import re
@@ -10,6 +11,7 @@ sys.path.insert(0, 'utils/')
 from get_defs import getWordDefinition
 from filter_word import isValidWord
 from filter_word import isValidDefinition
+from filter_word import hasGenderedTerm
 import pandas as pd
 
 
@@ -19,30 +21,24 @@ with open('words/all.json') as f:
 	for entry in all:
 		word = entry['word']
 		wordObj[word] = entry
-
-with open('words/word-sets.json') as f:
-	allSets = json.load(f)
 	
 def writeToJson(path, set):
 	with open(path + '.json', 'w') as outfile:
 		json.dump(set, outfile)
 
 def getSynonyms(word):
-	synonymsOne = vb.synonym(word)
-	synonymsTwo = []
-	if isinstance(synonymsOne, list):
-		synonymsOne = json.loads(synonyms)
-		synonymsOne = [synonym['text'] for synonym in synonyms]
+	syns = set()
+	result = vb.synonym(word)
+	if isinstance(result, list):
+		result = json.loads(result)
+		syns.update([synonym['text'] for synonym in result])
+	result = []
 	for syn in wordnet.synsets(word):
 		for l in syn.lemmas():
-			synonymsTwo.append(l.name())
-	if isinstance(synonymsTwo, list) and isinstance(synonymsOne, list):
-		return list(set().union(synonymsOne, synonymsTwo))
-	elif isinstance(synonymsTwo, list):
-		return synonymsTwo
-	elif isinstance(synonymsOne, list):
-		return synonymsOne
-	return ' '
+			syns.add(l.name())
+	w = Word(word)
+	syns.update(w.synonyms())
+	return syns
 
 def isNoun(word):
 	for ss in wordnet.synsets(word):
@@ -62,12 +58,6 @@ def isGendered(word, gender):
 		termsInString = pattern.search(definition)
 		if termsInString is not None:
 			if isValidDefinition(definition, termsInString.start(0), termsInString.end(0)):
-				all.append({
-					'word': word,
-					'definition': definition,
-					'gender': gender
-				})
-				wordsInSet.add(word)
 				return True
 	return False
 
@@ -98,7 +88,7 @@ def createSets(words):
 		synonyms_set = set([word])
 		synonyms = getSynonyms(word)
 		# add it's synonyms to set
-		if (synonyms != ' ' and synonyms != []):
+		if (synonyms):
 			for syn in synonyms:
 				syn = syn.lower()
 				if syn in wordsInSet:
@@ -107,9 +97,17 @@ def createSets(words):
 					syn = result[0]
 					if sameGender:
 						synonyms_set.add(syn)
+						continue
 				elif syn not in wordsInSet:
-					if isNoun(syn) and isGendered(syn, gender) and isSameGender(word, gender, syn)[1]:
+					if hasGenderedTerm(word) or (isNoun(syn) and isGendered(syn, gender) and isSameGender(word, gender, syn)[1]):
 						synonyms_set.add(syn)
+						all.append({
+							'word': word,
+							'definition': definition,
+							'gender': gender
+						})
+						wordsInSet.add(word)
+						continue
 			if len(synonyms_set) > 1:
 				allSets.append(list(synonyms_set))
 	return allSets
@@ -129,3 +127,5 @@ def addSetsToWords(words, wordSets):
 if __name__ == "__main__":
 	addSetsToWords(all, allSets)
 	writeToJson('words/all', all)
+	print(getSynonyms('woman of the streets'))
+	print(getSynonyms('sex kitten'))
